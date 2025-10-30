8e6b158447582983ba22e77b3da5ade15af4d7a7
Hernandez Hugo <21000385@alumnos.utleon.edu.mx>
2025-10-30 15:40:49 -0600

    figuras

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request # type: ignore
+import forms, math # type: ignore
 
 app = Flask(__name__)
- 
+#Figuras  - - - - -
+@app.route('/figuras', methods=['GET','POST'])
+def figuras():
+    form = forms.FiguraForm(request.form)
+    area = ""
+    figura = ""
+    if request.method=='POST' and form.validate():
+        figura = request.form.get('figura')
+        num1 = form.num1.data
+        num2 = form.num2.data
+        if figura == 'triangulo': area = (num1 * num2) / 2
+        elif figura == 'rectangulo': area = num1 * num2
+        elif figura == 'circulo': area = math.pi * (num1 ** 2)
+        elif figura == 'pentagono': area = (5 * num1 * num2) / 2
+    return render_template('figuras.html', form=form, area=area, figura=figura)
 @app.route('/')
+#HOME - - - - -
 def home():
     return "Hello, World!"
 #INDICE
@@ -24,10 +40,25 @@ def operas():
 @app.route('/distancia')
 def distancia():
     return render_template('distancia.html')
-
+#LAYOUT
 @app.route('/layout')
 def layout():
     return render_template('layout.html')
+#AlUMNOS
+@app.route('/alumnos', methods=['GET', 'POST'])
+def alumnos():
+    mat=0
+    nom=""
+    ape=""
+    em=""
+    alumnos_clase=forms.UserForm(request.form)
+    if request.method == 'POST' and alumnos_clase.validate():
+        mat=alumnos_clase.matricula.data
+        nom=alumnos_clase.nombre.data
+        ape=alumnos_clase.apellido.data
+        em=alumnos_clase.email.data   
+    return render_template('alumnos.html', form= alumnos_clase, mat=mat, nom=nom, ape=ape, em=em) 
+        
 
 @app.route('/about')
 def about():
